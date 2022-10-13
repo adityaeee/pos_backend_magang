07637fd179f60566779d15b36743f9351c3fb7e4
= <rifkyaditya966@gmail.com>
2022-10-13 17:18:47 +0700

    test

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.use("/test", indexRouter);
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
 app.use("/purchaseItem", purchaseItemRouter);
